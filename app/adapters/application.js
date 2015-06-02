aca7acb81d9698185704ee9e4b3540301432f102
alexkk <alexkk.cheung@gmail.com>
2015-06-02 22:40:54 +0800

    todoMvc using resful adapter.

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,16 @@
 import DS from 'ember-data';
 
-// export default DS.RESTAdapter.extend({
-//export default DS.FixtureAdapter.extend({
+export default DS.RESTAdapter.extend({
+  host: 'https://localhost/f3App'
+)}
+
+/*
+export default DS.FixtureAdapter.extend({
+({
+*/
+
+/*
 export default DS.LSAdapter.extend({
   namespace: 'extending-todomvc'
 });
+*/
